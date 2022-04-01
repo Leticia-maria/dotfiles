e18588d6b9096a804b27ccd2a5d2020fa0631661
Letícia Maria Pequeno Madureira <leticiapequeno30@gmail.com>
2022-04-01 11:30:38 -0300

    Update dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -1,2 +1,6 @@
 sudo apt-get install curl
 sudo apt-get install neovim
+
+## add vim-plug
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
